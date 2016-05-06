153e190800edd53f6a254ce6e5692aff03044348
Xavier <Xavier@Storm>
2016-05-06 17:11:19 -0500

    Se actualizaron las consultas sql.

--- src/java/controlador/Conexion.java
+++ src/java/controlador/Conexion.java
@@ -184,8 +184,6 @@ public class Conexion {
                                     "where idusuario ="+id+"");
             stmt.executeUpdate("delete from viaje\n" +
                                     "where idruta = (select idruta from ruta where idusuario = "+id+")");
-            stmt.executeUpdate("delete from ruta\n" +
-                                    "where idusuario ="+id+"");
             stmt.executeUpdate("UPDATE ruta/n" +
                                     "SET cupo = cupo - 1"+"WHERE idusuario ="+id+"");
              
